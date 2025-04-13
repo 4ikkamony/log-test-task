1a5536a3dbb0e4942af7c257d6d1c549eec9a00b
4ikkamony <compbanderloga@gmail.com>
2025-04-13 17:48:52 +0300

    Refactor error message display logic
    Since at least one device got a duplicate error message, list of
    messages had two identical entries.
    Possible ways to handle that:
      - Keep it as it is
      - Keep only unique messages
      - Track message count
    I chose the last approach

--- task_1/do_it_yourself.py
+++ task_1/do_it_yourself.py
@@ -1,9 +1,9 @@
 import re
-from typing import Callable, Optional
 from collections import defaultdict
 from dataclasses import dataclass
-from pathlib import Path
 from enum import Enum
+from pathlib import Path
+from typing import Callable, Optional
 
 from tqdm import tqdm
 
@@ -21,17 +21,17 @@ ERROR_MESSAGES = {
     0: "Battery device error",
     1: "Temperature device error",
     2: "Threshold central error",
-    3: "Unknown device error"
+    3: "Unknown device error",
 }
+
+
+class DeviceState(Enum):
+    OK = "02"
+    FAILED = "DD"
 
 
 class InvalidLogMessageError(Exception):
     pass
-
-
-class State(Enum):
-    OK = "02"
-    FAILED = "DD"
 
 
 @dataclass
@@ -51,61 +51,55 @@ class ReportDTO:
 #   4: STATE
 LOG_PATTERN = (
     r"'"
-      r"BIG;"
-      r"-?[0-9]+;"
-      r"([0-9a-zA-Z]{6});" # ID
-      r"(?:[0-9]+;){3}"
-      r"(-?[0-9]{4});" # S_P_1
-      r"(?:-?[0-9]+;){6}"
-      r"(-?[0-9]{3});" # S_P_2
-      r"(?:-?[0-9]+;){3}"
-      r"(02|DD);" # STATE
-    r"'")
+    r"BIG;"
+    r"-?[0-9]+;"
+    r"([0-9a-zA-Z]{6});"  # ID
+    r"(?:[0-9]+;){3}"
+    r"(-?[0-9]{4});"  # S_P_1
+    r"(?:-?[0-9]+;){6}"
+    r"(-?[0-9]{3});"  # S_P_2
+    r"(?:-?[0-9]+;){3}"
+    r"(02|DD);"  # STATE
+    r"'"
+)
 
 
-def _parse_message(line: str) -> tuple[str, str, str, State]:
-    """"Excract HANDLER, ID, S_P_1, S_P_2 and STATE form log line"""
+def _parse_message(line: str) -> tuple[str, str, str, DeviceState]:
+    """Extract ID, S_P_1, S_P_2 and STATE form log line"""
+
     match = re.search(LOG_PATTERN, line)
 
     if not match:
         raise InvalidLogMessageError(f"Could not match {line}")
 
     device_id = str(match.group(1))
-    s_p_1 = (match.group(2))
-    s_p_2 = (match.group(3))
-    state = State(match.group(4))
+    s_p_1 = match.group(2)
+    s_p_2 = match.group(3)
+    state = DeviceState(match.group(4))
 
     return device_id, s_p_1, s_p_2, state
 
 
 def _get_error_messages(s_p_1: str, s_p_2: str) -> list[str]:
-    """Process messages with STATUS=DD and figure out error messages"""
-    s_p_1 = s_p_1[:-1] # get rid of control sum
+    """Get list of error messages, based on s_p_1 and s_p_2(values parsed from log)"""
+
+    s_p_1 = s_p_1[:-1]  # get rid of control sum
     status_str = s_p_1 + s_p_2
 
-    pairs = [status_str[i:i + 2] for i in range(0, len(status_str), 2)]
+    pairs = [int(status_str[i : i + 2]) for i in range(0, len(status_str), 2)]
 
     # a flag is 4-th bit from the right of binary representation of a pair(8 bit total)
-    flags: list[int] = [
-        int(
-            bin(int(pair))[2:].zfill(8)[4]
-        )
-        for pair in pairs
-    ]
+    flags: list[int] = [int(bin(pair)[2:].zfill(8)[4]) for pair in pairs]
 
-    error_messages = [
-        ERROR_MESSAGES[i]
-        for i, flag in enumerate(flags)
-        if flag == 1
-    ]
+    error_messages = [ERROR_MESSAGES[i] for i, flag in enumerate(flags) if flag == 1]
 
     return error_messages if error_messages else [ERROR_MESSAGES[3]]
 
 
 def _init_report_dto() -> ReportDTO:
     return ReportDTO(
-        ok_counts_by_device_id = defaultdict(int),
-        error_messages_by_device_id = defaultdict(list)
+        ok_counts_by_device_id=defaultdict(int),
+        error_messages_by_device_id=defaultdict(dict),
     )
 
 
@@ -123,6 +117,8 @@ def _get_report_from_logs(
             if loop_limit and i > loop_limit:
                 break
 
+            # skip irrelevant lines
+            # faster than applying regex right away
             if "BIG" not in line:
                 continue
 
@@ -134,7 +130,7 @@ def _get_report_from_logs(
                 print(exception)
                 continue
 
-            if state == State.OK:
+            if state == DeviceState.OK:
                 report.total_ok_count += 1
                 if device_id not in report.error_messages_by_device_id:
                     report.ok_counts_by_device_id[device_id] += 1
@@ -143,22 +139,46 @@ def _get_report_from_logs(
                 if device_id in report.ok_counts_by_device_id:
                     del report.ok_counts_by_device_id[device_id]
 
+                # some error messages appear more than one time
+                # I prefer tracking appearance count over ignoring duplicates
                 error_messages = _get_error_messages(s_p_1, s_p_2)
-                report.error_messages_by_device_id[device_id].extend(error_messages)
+                for error_message in error_messages:
+                    count = report.error_messages_by_device_id[device_id].get(
+                        error_message, 0
+                    )
+                    report.error_messages_by_device_id[device_id][error_message] = (
+                        count + 1
+                    )
+
     return report
 
 
 def _display_report(report: ReportDTO) -> None:
     print(f"{'=' * 36}Report{'=' * 36}")
-    print(f"Total 'BIG' messages     : {report.total_big_count}")
-    print(f"Successful messages   : {report.total_ok_count}")
-    print(f"Failed messages       : {report.total_failed_count}")
+
+    print()
+    print(
+        f"TOTAL OK DEVICES: {len(report.ok_counts_by_device_id)}"
+        f" | "
+        f"TOTAL FAILED DEVICES: {len(report.error_messages_by_device_id)}"
+    )
+    print()
+
+    print("=" * 36)
+
+    print(f"Total 'BIG' messages:      {report.total_big_count}")
+    print(f"Successful 'BIG' messages: {report.total_ok_count}")
+    print(f"Failed 'BIG' messages:     {report.total_failed_count}")
     print()
 
     if report.error_messages_by_device_id:
         print("Errors per device:")
         for device_id, error_messages in report.error_messages_by_device_id.items():
-            print(f"  {device_id}: {", ".join(error_messages)}")
+            error_messages_str = ", ".join(
+                f"{msg}({count} times)" if count > 1 else msg
+                for msg, count in error_messages.items()
+            )
+            print(f"  {device_id}: {error_messages_str}")
         print()
 
     if report.ok_counts_by_device_id:
@@ -167,14 +187,6 @@ def _display_report(report: ReportDTO) -> None:
             print(f"  {device_id}: {count}")
         print()
 
-    print("=" * 36)
-    print(
-        f"TOTAL OK DEVICES: {len(report.ok_counts_by_device_id)}"
-        f" | "
-        f"TOTAL FAILED DEVICES: {len(report.error_messages_by_device_id)}"
-    )
-    print("=" * 36)
-
 
 def main(
     filepath: str | Path = LOG_FILE_PATH,
@@ -182,9 +194,7 @@ def main(
     report_initializer: Callable[..., ReportDTO] = _init_report_dto,
 ) -> None:
     report = _get_report_from_logs(
-        filepath=filepath,
-        loop_limit=loop_limit,
-        report_initializer=report_initializer
+        filepath=filepath, loop_limit=loop_limit, report_initializer=report_initializer
     )
 
     _display_report(report)
